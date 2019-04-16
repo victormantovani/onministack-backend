d659c0de1c73245827caf883aa3fedf35106c66b
Mantovs <victor.mantovani@iv2.com.br>
2019-04-16 00:23:13 -0300

    ok

--- src/server.js
+++ src/server.js
@@ -33,4 +33,4 @@ app.use("/files", express.static(path.resolve(__dirname,'..','tmp')));
 
 app.use(require("./routes"));
 
-app.listen(process.env.PORT || "3333");
+server.listen(process.env.PORT || "3333");
